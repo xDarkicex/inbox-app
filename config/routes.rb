a30c7dafed1ac0429566c893202ab02216258f01
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-05 10:58:21 -0700

    conversation controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   get 'mailbox/inbox' => "mailbox#inbox", as: :mailbox_inbox
   get 'mailbox/sent' => "mailbox#sent", as: :mailbox_sent
   get 'mailbox/trash' => "mailbox#trash", as: :mailbox_trash
-
+  resources :conversations do
+    member do
+      post :reply
+      post :trash
+      post :untrash
+    end
+  end
 end
