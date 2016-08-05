0bcc2a752e1a189d654004eeb25c301cc2b09fb9
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-05 10:25:25 -0700

    add inbox counter

--- app/helpers/mailbox_helper.rb
+++ app/helpers/mailbox_helper.rb
@@ -1,2 +1,7 @@
 module MailboxHelper
+  def unread_messages_count
+    #capture amount of unread messages
+    #using Mailboxer
+    mailbox.index(unread: true).count(:id, distinct: true)
+  end
 end
