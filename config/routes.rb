0855b74132d0397db6a83ff85110be493258be43
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-04 23:49:22 -0700

    adding custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'mailbox/inbox'
-
-  get 'mailbox/sent'
-
-  get 'mailbox/trash'
 
   devise_for :users
   root 'welcome#index'
+  get 'mailbox/inbox' => "mailbox#inbox", as: :mailbox_inbox
+  get 'mailbox/sent' => "mailbox#sent", as: :mailbox_sent
+  get 'mailbox/trash' => "mailbox#trash", as: :mailbox_trash
 
 end
